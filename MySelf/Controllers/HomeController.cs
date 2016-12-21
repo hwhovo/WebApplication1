f12e503ba8574c14e47a12c376f2d207230cecf8
hwhovo <hwhovo@gmail.com>
2016-12-21 23:57:43 +0400

    Adding pagination and filtration

--- MySelf/Controllers/HomeController.cs
+++ MySelf/Controllers/HomeController.cs
@@ -3,28 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PagedList;
+using MySelf.Models;
+using System.Data.Entity;
 
 namespace MySelf.Controllers
 {
-    public class HomeController : Controller
+    public class MyController : Controller
     {
-        public ActionResult Index()
+        // GET: Home
+        public ActionResult Index(int? page)
         {
-            return View();
+            int pageNumber = page ?? 1;
+            int pageSize = 3;
+
+            
+
+            return View(phones.ToPagedList(pageNumber, pageSize));
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
+        
 
-            return View();
+        List<Phone> phones;
+        public MyController()
+        {
+            phones = new List<Phone>();
+            phones.Add(new Phone { Id = 1, Model = "Samsung Galaxy III", Producer = "Samsung" });
+            phones.Add(new Phone { Id = 2, Model = "Samsung Ace II", Producer = "Samsung" });
+            phones.Add(new Phone { Id = 3, Model = "HTC Hero", Producer = "HTC" });
+            phones.Add(new Phone { Id = 4, Model = "HTC One S", Producer = "HTC" });
+            phones.Add(new Phone { Id = 5, Model = "HTC One X", Producer = "HTC" });
+            phones.Add(new Phone { Id = 6, Model = "LG Optimus 3D", Producer = "LG" });
+            phones.Add(new Phone { Id = 7, Model = "Nokia N9", Producer = "Nokia" });
+            phones.Add(new Phone { Id = 8, Model = "Samsung Galaxy Nexus", Producer = "Samsung" });
+            phones.Add(new Phone { Id = 9, Model = "Sony Xperia X10", Producer = "SONY" });
+            phones.Add(new Phone { Id = 10, Model = "Samsung Galaxy II", Producer = "Samsung" });
         }
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
+
 }
